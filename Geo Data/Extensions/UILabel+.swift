f2d6611b96dbdf65e972e048c4acac75ba1e68d6
Denis Pavlov <akcel322@gmail.com>
2023-05-24 20:51:13 +0700

    Дополнение к предыдущему коммиту

---
+++ Geo Data/Extensions/UILabel+.swift
@@ -0,0 +1,18 @@
+//
+//  UILabel+.swift
+//  Geo Data
+//
+//  Created by Денис Павлов on 24.05.2023.
+//
+
+import UIKit
+
+extension UILabel {
+    func createEmptyTextFieldLabel(text: String) -> Self {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.alpha = 0
+        self.text = text
+        self.font = UIFont.systemFont(ofSize: 14)
+        return self
+    }
+}
